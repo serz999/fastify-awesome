a0e4e185cb265c29223c1a78d17adc0df80a7268
Andrei <andrei.green@mail.ru>
2023-06-22 20:05:16 +0300

    change DB name and password. add jwt-fastify folder with some code(not working)

---
+++ jwt-fastify/middleware/auth_middleware.js
@@ -0,0 +1,11 @@
+const fastifyPlugin = require('fastify-plugin');
+
+module.exports = fastifyPlugin(async (fastify)=>{
+    fastify.decorate('jwtauthentication', async (req,res)=>{
+        try {
+            await req.jwtVerify();
+        } catch (error) {
+            res.send(error);
+        }
+    })
+})
